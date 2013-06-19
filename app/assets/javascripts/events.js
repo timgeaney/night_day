5c396aa36cf9b0475da686c240f652afc013174a
Tim Geaney <tim@Tim.local>
2013-06-19 15:11:55 +0100

    added jquery fliipy to events

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,12 +1,33 @@
 
 $(window).load(function() {
 
-  var $container = $('#events');
-// initialize
-  $container.masonry({
-    "isOriginLeft": false,
-    itemSelector: '.item'
-});
+    var $container = $('#events');
+//   initialize
+    $container.masonry({
+      itemSelector: '.item'
+  });
 
+  $(function(){
+
+    $("#reverse").on("click",function(e){
+        $(".flipbox").flippyReverse();
+        e.preventDefault();
+    });
+
+  	  $("#flip").on("click", function(e){
+  	    $(".flipbox").flippy({
+        color_target: "white",
+        duration: "1000",
+        verso: "woohoo"
+  	  });
+  	    e.preventDefault();
+  	  
+   });
+
+
+
+
+  });
+    
 });
 
